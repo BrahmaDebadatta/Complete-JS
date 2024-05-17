9a22fe3f9ec4398497b5ecf70ef4203911babcaa
Debadatta Brahma <104821127+BrahmaDebadatta@users.noreply.github.com>
2024-05-17 16:27:14 +0000

    Datatypes In Depth

---
+++ Basics/DatatypesInDepth.js
@@ -0,0 +1,30 @@
+// There are 2 DataTypes in Js
+//1. Primitive  2. Non Primitive or Referece Type
+
+// There are 7 Primitive Datatype 
+//1. Number, 2. String, 3. Boolean , 4. Undefind, 5. null, 6. Symbol, 7. BigInt
+
+let number = 45// number
+let Name  = "Debadatta" // string
+let isDeveloper = true // Booelean
+let haveLotMoney = undefined // undefind
+let ChocoletBox = null // null
+console.log(typeof null);// object
+//Imp : the typeof null is Object 
+let id = Symbol("1223")
+let NewId = Symbol("1223")
+// symbol is used to assign unique value and here both id and NewId are unique although having same value
+
+console.log(id === NewId); // false
+
+let bignum = 5667788876876768n // bigInt 
+
+
+// Reference Type 
+// Array, Objects, Functions
+
+
+
+
+
+
