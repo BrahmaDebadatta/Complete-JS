b835d5f1bfd5a2f59ee5061ba4b84c543d087066
Debadatta Brahma <104821127+BrahmaDebadatta@users.noreply.github.com>
2024-05-17 15:27:28 +0000

    ConverSion In JS

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -13,3 +13,15 @@ let age_2 = "18abc"
 let Convert_age_2 = Number(age_2)
 console.log(typeof Convert_age_2)// converted to number type
 console.log(Convert_age_2);// but here when we converted "18abc" to number it is converted But the o/p will be NaN i.e not a number
+//===================================
+
+let num = 6
+let negValue  = -num
+console.log(negValue)
+
+let Fname = "debadatta"
+let Lname = " brahma"
+let res = Fname + Lname
+let res2 = Fname - Lname
+console.log(res);//Op will be debadatta brahma
+console.log(res2);// op will be NaN
