6909c7be48ab7b6b2171516193cab690abec7a03
Debadatta Brahma <104821127+BrahmaDebadatta@users.noreply.github.com>
2024-05-17 15:42:25 +0000

    Comparison In JS

---
+++ Basics/Comparison.js
@@ -0,0 +1,13 @@
+console.log("5" < 9); // O/p will be true , internally "5" will be Converted to Number
+
+console.log(undefined > 0); // False
+console.log(undefined == 0); // False
+console.log(undefined >= 0); // False
+
+
+console.log(null > 0); // False
+console.log(null == 0); // False
+console.log(null >= 0);//True
+
+console.log("19" == 19);// true BCZ here it only check the Value not the Data Type
+console.log("19" === 19);//false BCZ here it  check the Value as well as the  the Data Type
