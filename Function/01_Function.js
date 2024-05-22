ddb6a447a7a430d0a3feb402fe2abaa4cf8d40b6
Debadatta Brahma <104821127+BrahmaDebadatta@users.noreply.github.com>
2024-05-22 15:59:56 +0000

    Function basics

---
+++ Function/01_Function.js
@@ -0,0 +1,90 @@
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+
+
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+console.log("Hi dev");
+
+
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+
+
+function hiDev(){
+    console.log("Hi dev");
+    console.log("Hi dev");
+    console.log("Hi dev");
+    console.log("Hi dev");
+    console.log("Hi dev");
+    console.log("Hi dev");
+}
+
+console.log("function call")
+hiDev();
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+console.log("function call")
+hiDev();
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+console.log("function call")
+hiDev();
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+// Hi dev
+
+//function is basically used to do a repitative task in a better and easy way
+// in above example you can see i have call hiDev() function 3 times and the output is 
+// same as writing console.log("Hi dev") 18 time .
+
+function addNum(num1, num2){ // here num1 and num2 is called as function parameters
+
+    console.log(num1+num2);
+
+}
+
+addNum(50,48) // here 50 and 48 is called as function arguments
